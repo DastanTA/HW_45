1c79575846e4d4cdc44e3124688db78e7e2a0ebe
Dastan T <dastan.turatbekov@gmail.com>
2022-11-04 18:48:51 +0600

    Main page is created + index.html structure made + url to main page is added

--- source/to_do/views.py
+++ source/to_do/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from to_do.models import ToDoList
 
-# Create your views here.
+
+def main_page(request):
+    list_to_do = ToDoList.objects.all()
+    context = {
+        'list_to_do': list_to_do
+    }
+    return render(request, 'index.html', context)
