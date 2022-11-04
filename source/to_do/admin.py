5408b8b04394cfa69af1e7e9a38322342749450f
Dastan T <dastan.turatbekov@gmail.com>
2022-11-04 18:03:36 +0600

    Database structure & superuser is created

--- source/to_do/admin.py
+++ source/to_do/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from to_do.models import ToDoList
 
-# Register your models here.
+
+class ToDoAdmin(admin.ModelAdmin):
+    list_display = ['description', 'status', 'deadline']
+    list_filter = ['status']
+    search_fields = ['description']
+    fields = ['id', 'description', 'status', 'deadline', 'created_add']
+    
+
+admin.site.register(ToDoList, ToDoAdmin)
